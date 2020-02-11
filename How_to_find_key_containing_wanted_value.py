8459f5a4f59f078cd08614b7644b23d4c7ec1ec2
Slantext <53126986+Slantext@users.noreply.github.com>
2020-02-11 11:05:49 -0800

    Add files via upload

--- How_to_find_key_containing_wanted_value.py
+++ How_to_find_key_containing_wanted_value.py
@@ -1,9 +1,13 @@
 thisdict = {'george':16,
             'amber':19}
-
-#thisdict["brand"].append(["Sub", "Red"])
-#print(thisdict["brand"])
-
 search_age = 16
+
+# Method 1
 for name, age in thisdict.items():
     print(list(thisdict.keys())[list(thisdict.values()).index(search_age)])
+
+
+# Method 2
+for name, age in thisdict.items():
+    if age == search_age:
+        print name
